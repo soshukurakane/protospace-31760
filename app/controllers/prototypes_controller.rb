2b62972d3dc1c7fef8809152ee5fc52797e2e546
soshukurakane <73089033+soshukurakane@users.noreply.github.com>
2020-11-10 11:36:08 +0900

    inital commit

---
+++ app/controllers/prototypes_controller.rb
@@ -0,0 +1,57 @@
+class PrototypesController < ApplicationController
+  before_action :authenticate_user!, except: [:index, :show,]
+
+  def index
+    @prototypes = Prototype.all
+  end
+
+  def create
+    @prototype = Prototype.create(prototype_params)
+      if @prototype.save
+        redirect_to root_path
+      else
+        render :new
+      end
+  end
+
+  def new
+    @prototype = Prototype.new
+  end
+
+  def show
+    @prototype = Prototype.find(params[:id])
+    @comment = Comment.new
+    @comments = @prototype.comments.includes(:user)
+    #@prototype.commentsとすることで、@prototypeへ投稿された全てのコメントを取得できる
+    #includes(:モデル名)で　N+1問題を解決できる
+  end
+
+  def edit
+    @prototype = Prototype.find(params[:id])
+    unless @prototype.user_id == current_user.id
+           # @prototype.user_idで、取得した情報が今ログインしているidと同じかどうか判断するための記述
+      redirect_to action: :index
+    end
+  end
+
+  def update
+    prototype = Prototype.find(params[:id])
+    if prototype.update(prototype_params)
+      redirect_to  prototype_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @prototype = Prototype.find(params[:id])
+    if @prototype.destroy
+      redirect_to  root_path
+    end
+  end
+
+  private
+  def prototype_params
+    params.require(:prototype).permit(:title, :catch_copy, :concept, :image).merge(user_id: current_user.id)
+  end
+end
